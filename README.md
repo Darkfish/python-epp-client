b8a60decf81edb972cb946b456d2a14b1d9cd713
Mike Gray <mike@nzrs.net.nz>
2017-02-21 10:49:03 +1300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # python-epp-client
-Minimalist script for manually executing EPP commands
+Minimalist script for manually executing EPP commands with client-certificate auth
+
+## Usage
+
+```
+$ python eppclient.py --server nzrsepp.vagrant.dev --certificate crt.pem --private-key key.pem login.xml info.xml 
+2017-02-21 10:37:54,475 INFO  - Making SSL connection to nzrsepp.vagrant.dev:700
+2017-02-21 10:37:54,544 INFO Trying to read EPP Greeting from server
+2017-02-21 10:37:54,544 INFO   - Trying to read 4-byte header from socket
+2017-02-21 10:37:54,625 INFO   - Found length header, trying to read 584 bytes
+2017-02-21 10:37:54,627 INFO <?xml version="1.0" encoding="UTF-8"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"><greeting><svID>nzrsepp.vagrant.dev</svID><svDate>2017-02-20T21:38:19+00:00</svDate><svcMenu><version>1.0</version><lang>en</lang><objURI>urn:ietf:params:xml:ns:domain-1.0</objURI><objURI>urn:ietf:params:xml:ns:contact-1.0</objURI><svcExtension><extURI>urn:ietf:params:xml:ns:secDNS-1.1</extURI></svcExtension></svcMenu><dcp><access><personalAndOther/></access><statement><purpose><admin/><prov/></purpose><recipient><ours/></recipient><retention><business/></retention></statement></dcp></greeting></epp>
+
+2017-02-21 10:37:54,627 INFO Sending login.xml
+2017-02-21 10:37:54,627 INFO Sending XML (556 bytes):
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+  <command>
+    <login>
+      <clID>999</clID>
+      <pw>asdfasdf</pw>
+      <options>
+        <version>1.0</version>
+        <lang>en</lang>
+      </options>
+      <svcs>
+        <objURI>urn:ietf:params:xml:ns:domain-1.0</objURI>
+        <objURI>urn:ietf:params:xml:ns:contact-1.0</objURI>
+        <svcExtension>
+          <extURI>urn:ietf:params:xml:ns:secDNS-1.1</extURI>
+        </svcExtension>
+      </svcs>
+    </login>
+  </command>
+</epp>
+
+2017-02-21 10:37:55,630 INFO   - Trying to read 4-byte header from socket
+2017-02-21 10:37:55,630 INFO   - Found length header, trying to read 238 bytes
+2017-02-21 10:37:55,630 INFO Response from server: 
+<?xml version="1.0" encoding="UTF-8"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"><response><result code="1000"><msg lang="en">Command completed successfully</msg></result><trID><svTRID>7,1323159525[1]+2</svTRID></trID></response></epp>
+
+2017-02-21 10:41:06,474 INFO Sending check.xml
+2017-02-21 10:41:06,474 INFO Sending XML (286 bytes):
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0">
+  <command>
+    <check>
+      <domain:check xmlns:domain="urn:ietf:params:xml:ns:domain-1.0">
+        <domain:name>nzrs.net.nz</domain:name>
+      </domain:check>
+    </check>
+    <clTRID>check-domain-0123</clTRID>
+  </command>
+</epp>
+
+2017-02-21 10:41:07,475 INFO   - Trying to read 4-byte header from socket
+2017-02-21 10:41:07,476 INFO   - Found length header, trying to read 439 bytes
+2017-02-21 10:41:07,476 INFO Response from server: 
+<?xml version="1.0" encoding="UTF-8"?>
+<epp xmlns="urn:ietf:params:xml:ns:epp-1.0"><response><result code="1000"><msg lang="en">Command completed successfully</msg></result><resData><domain:chkData xmlns:domain="urn:ietf:params:xml:ns:domain-1.0"><domain:cd><domain:name avail="0">nzrs.net.nz</domain:name></domain:cd></domain:chkData></resData><trID><clTRID>check-domain-0123</clTRID><svTRID>7,1323159583</svTRID></trID></response></epp>
+```
---
+++ eppclient.py
@@ -0,0 +1,151 @@
+import os
+import ssl
+import time
+import struct
+import socket
+import logging
+import argparse
+
+#: Log items
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s',
+                    level=logging.INFO)
+
+
+#: Parse arguments
+parser = argparse.ArgumentParser(
+    description='Send prepared XML files to EPP server'
+)
+parser.add_argument(
+    'xmlfiles',
+    metavar='xmlfile',
+    type=str,
+    nargs='+',
+    help='XML Input Files'
+)
+parser.add_argument(
+    '--server',
+    nargs='?',
+    default='srstestepp.srs.net.nz',
+    help='EPP server hostname or ip'
+)
+parser.add_argument(
+    '--certificate',
+    nargs='?',
+    default=os.path.join(
+        os.getcwd(), 'Certs/cert.pem'
+    ),
+    help='client certificate file'
+)
+parser.add_argument(
+    '--private-key',
+    nargs='?',
+    default=os.path.join(
+        os.getcwd(), 'Certs/key.pem'
+    ),
+    help='private key file'
+)
+parser.add_argument(
+    '--ca-certificate',
+    nargs='?',
+    default=os.path.join(
+        os.getcwd(), 'Certs/nzrs-ca-chain.pem'
+    ),
+    help='file with the bundle of CAs'
+)
+args = parser.parse_args()
+
+
+
+class epp(object):
+    def __init__(self):
+        #: Set host
+        self.host = args.server
+        self.port = 700
+
+        #: Find size of C integers
+        self.format_32 = self.format_32()
+
+        #: Create connection to EPP server
+        logging.info(' - Making SSL connection to {0}:{1}'.format(
+            self.host,
+            self.port
+        ))
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.settimeout(301)
+        self.socket.connect((self.host, self.port))
+        self.ssl = ssl.wrap_socket(
+            self.socket,
+            certfile=args.certificate,
+            keyfile=args.private_key,
+            ca_certs=args.ca_certificate
+        )
+
+    def __del__(self):
+        try:
+            self.socket.close()
+        except TypeError:
+            """ Will occur when not properly connected """
+            pass
+
+    #: Uses basic EPP data structures from:
+    #  - https://github.com/jochem/Python-EPP
+    #  - http://www.bortzmeyer.org/4934.html
+    def format_32(self):
+        # Get the size of C integers. We need 32 bits unsigned.
+        format_32 = ">I"
+        if struct.calcsize(format_32) < 4:
+            format_32 = ">L"
+            if struct.calcsize(format_32) != 4:
+                raise Exception("Cannot find a 32 bits integer")
+        elif struct.calcsize(format_32) > 4:
+            format_32 = ">H"
+            if struct.calcsize(format_32) != 4:
+                raise Exception("Cannot find a 32 bits integer")
+        else:
+            pass
+        return format_32
+
+    def int_from_net(self, data):
+        return struct.unpack(self.format_32, data)[0]
+
+    def int_to_net(self, value):
+        return struct.pack(self.format_32, value)
+
+    def read(self, schema_critical=True):
+        logging.info('  - Trying to read 4-byte header from socket')
+        length = self.ssl.read(4)
+        if length:
+            i = self.int_from_net(length)-4
+            logging.info(
+                '  - Found length header, trying to read {0} bytes'.format(i)
+            )
+
+            #: Return raw data
+            return(self.ssl.read(i))
+
+    def write(self, xml):
+        epp_as_string = xml
+        # +4 for the length field itself (section 4 mandates that)
+        # +2 for the CRLF at the end
+        length = self.int_to_net(len(epp_as_string) + 4 + 2)
+        logging.info(
+            'Sending XML ({0} bytes):\n'.format(
+                len(epp_as_string) + 4 + 2) + xml
+        )
+        self.ssl.send(length)
+
+        return self.ssl.send(epp_as_string + "\r\n")
+
+#: Connect to EPP server
+client = epp()
+
+#: Fetch greeting
+logging.info('Trying to read EPP Greeting from server')
+logging.info(client.read())
+
+for fname in args.xmlfiles:
+    with open(fname, 'r') as f:
+        logging.info('Sending {0}'.format(fname))
+        client.write(f.read())
+        time.sleep(1)
+        logging.info('Response from server: \n{0}'.format(client.read()))
